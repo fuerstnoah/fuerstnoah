fba014824d2a54f96856ae48d967dae264064ef4
fuerstnoah <82971532+fuerstnoah@users.noreply.github.com>
2022-07-05 00:36:39 +0200

    Update and rename Aufgabe5_4.java to SmoothMatrix.java

--- OOSE/SmoothMatrix.java
+++ OOSE/SmoothMatrix.java
@@ -7,7 +7,7 @@
  *
  * @author Noah
  */
-public class Aufgabe5_4{
+public class SmoothMatrix{
 
     public static void main(String[] args){
         long[][] matrix = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
