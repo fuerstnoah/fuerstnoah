72277a80f83308cdcd17aa375c3ff7ffbd9b3bf0
fuerstnoah <82971532+fuerstnoah@users.noreply.github.com>
2022-07-05 00:23:39 +0200

    Add files via upload

---
+++ OOSE/Uebung8/src/main/java/IMyComparable.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+/**
+ *
+ * @author Noah
+ */
+public interface IMyComparable {
+    //return 0 for even positive for bigger and negative for smaller
+    int compareTo(Object obj);
+}
